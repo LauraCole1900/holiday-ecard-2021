6318d9be7226adcad24d461830dacb75e094ce9c
Laura Cole <lauracole1900@comcast.net>
2021-12-19 22:15:54 -0700

    added carolNames arr and rearranged other variables re: random()

--- src/pages/lander.js
+++ src/pages/lander.js
@@ -7,17 +7,16 @@ import { Button, Col, Container, Row } from "react-bootstrap";
 
 const Lander = () => {
   const characters = ["Santa Claus", "Jack Frost", "Frosty", "Abominable Snowmonster", "Rudolph", "the reindeer", "an elf", "Baby New Year"];
+  const carolNames = ["in the bleak midwinter", "silent night", "away in a manger"]
   const nouns = [];
   const verbs = [];
   const adjectives = [];
   const adverbs = [];
-  let randomIndex = 0;
-  let word = "";
   let chosenWords = [];
 
   const random = (arr) => {
-    randomIndex = Math.floor(Math.random() * arr.length);
-    word = arr[randomIndex];
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    const word = arr[randomIndex];
     chosenWords.push(word);
     // return word;
   }
