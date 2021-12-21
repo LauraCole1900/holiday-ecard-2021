f893608e56ca2c23e70c4c9912c0989772fa21d3
Laura Cole <lauracole1900@comcast.net>
2021-12-21 11:56:37 -0700

    added react-snowfall, starrySky, & animejs for starrySky

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { Lander } from "./pages";
+import Snowfall from "react-snowfall";
+import StarrySky from "./components/starry-sky";
 import './App.css';
 
 function App() {
   return (
-    <Lander />
+    <>
+      <Snowfall />
+      <StarrySky />
+      <Lander />
+    </>
   );
 }
 
