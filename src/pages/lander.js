0c40d1c412729eb400420c62f4460d97ab8946c2
Laura Cole <lauracole1900@comcast.net>
2021-12-20 23:43:26 -0700

    working on word arrays

--- src/pages/lander.js
+++ src/pages/lander.js
@@ -6,9 +6,10 @@ import { Button, Col, Container, Row } from "react-bootstrap";
 // Loop through word array to populate Mad Lib
 
 const Lander = () => {
-  const characters = ["Santa Claus", "Jack Frost", "Frosty", "Abominable Snowmonster", "Rudolph", "the reindeer", "an elf", "Baby New Year"];
+  const characters = ["Santa Claus", "Mrs. Claus", "Jack Frost", "Frosty", "Abominable Snowmonster", "Rudolph", "the reindeer", "the kangaroos", "an elf", "Baby New Year", "Scrooge", "Clara", "Nutcracker Prince", "Sugar Plum Fairy"];
   const carolNames = ["in the bleak midwinter", "silent night", "away in a manger"]
-  const nouns = [];
+  const nouns = ["pine tree", "eggnog", "Yule log", "lights", "candles", "wreath", "garland", "poinsettia", "cookies", "carrots", "scarf", "hat", "stockings", "skis", "menorah", "dreidl", "latkes", "gelt", "sufganiyah", "hot chocolate", "candy cane", "fruitcake", "holly", "mistletoe", "bells", "gingerbread house", "ribbons", "presents", "kinara", "mishumaa sabaa", "kikombe cha umoja", "mkeka", "mazao", "muhindi", "zawadi", "nutcracker", "snow", "sleigh", "snowflakes", "ribbons", "tinsel", "ornaments"];
+  const colors = ["red", "green", "white", "silver", "gold", "black"]
   const verbs = [];
   const adjectives = [];
   const adverbs = [];
